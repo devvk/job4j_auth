a63b42f7d14319e9caad660cb220fadee6a67cca
devvk <info@sekretov.net>
2026-06-05 19:24:16 +0200

    1. Spring Liquibase [#302330 #564603]

--- src/main/java/ru/job4j/auth/PersonsApplication.java
+++ src/main/java/ru/job4j/auth/PersonsApplication.java
@@ -1,14 +1,25 @@
 package ru.job4j.auth;
 
+import liquibase.integration.spring.SpringLiquibase;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
 public class PersonsApplication {
+
+    @Bean
+    public SpringLiquibase liquibase(DataSource dataSource) {
+        SpringLiquibase liquibase = new SpringLiquibase();
+        liquibase.setChangeLog("classpath:db/dbchangelog.xml");
+        liquibase.setDataSource(dataSource);
+        return liquibase;
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(PersonsApplication.class, args);
         System.out.println("http://localhost:8080/persons");
 	}
-
 }
